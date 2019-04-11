50a7a7725b4651ad4c8d5d43cb85f00527fd94a2
Ragimzade <n.ragimzade1@gmail.com>
2019-04-11 19:43:48 +0300

    кодирование

--- sandbox/src/main/java/First.java
+++ sandbox/src/main/java/First.java
@@ -1,6 +1,18 @@
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+       hello("Java");
+       hello("user");
+
+       double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area(double l){
+        return l * l;
     }
 }
