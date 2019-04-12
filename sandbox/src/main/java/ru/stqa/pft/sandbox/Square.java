3963210cf744c262e86af6a3f9c0f6cebdd2fe44
Ragimzade <n.ragimzade1@gmail.com>
2019-04-12 15:34:29 +0300

    15. Методы, функции, ассоциированные с объектами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -3,6 +3,10 @@ package ru.stqa.pft.sandbox;
 public class Square {
     public double l;
 
+    public double area() {
+        return this.l * this.l;
+    }
+
     public Square(double l) {
         this.l = l;
     }
